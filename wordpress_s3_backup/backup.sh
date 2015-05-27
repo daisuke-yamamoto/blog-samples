466099265dc98b25b2a79d5826c1b2054b09a33e
yusukemurayama <yusuke@remotestance.com>
2015-05-27 16:49:25 +0900

    wordpress_s3_backup用のコードを追加しました。

---
+++ wordpress_s3_backup/backup.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+AWS_BIN=/usr/bin/aws
+MYSQLDUMP_BIN=/usr/bin/mysqldump
+
+# 保存先のバケット名
+AWS_BUCKET_NAME=<BUCKET_NAME>
+
+# MySQLのユーザ名
+MYSQL_USER=<MYSQL_USERNAME>
+# MySQLのホスト
+MYSQL_HOST=<MYSQL_HOST>
+# MySQLのパスワード
+MYSQL_PASS=<MYSQL_PASSWORD>
+# MySQLのDB名
+MYSQL_DBNAME=<MYSQL_DBNAME>
+# MySQLの、バックアップの一時保存先
+MYSQL_TEMPPATH=/tmp/dump.sql
+# MySQLの、S3の保存先パス
+MYSQL_S3_DESTPATH=mysql/
+
+# アップロードされた画像が保存されているディレクトリのパス
+UPLOADED_IMAGE_DIR=<WORDPRESS_PATH>/wp-content/uploads/
+# アップロードされた画像の、S3の保存先パス
+UPLOADED_IMAGE_S3_DESTPATH=uploaded_images/
+
+# MySQLをS3にバックアップ
+$MYSQLDUMP_BIN -cu $MYSQL_USER -h $MYSQL_HOST --password=$MYSQL_PASS $MYSQL_DBNAME > $MYSQL_TEMPPATH
+$AWS_BIN s3 cp $MYSQL_TEMPPATH s3://$AWS_BUCKET_NAME/$MYSQL_S3_DESTPATH
+
+# 画像をS3にバックアップ
+$AWS_BIN s3 sync --delete $UPLOADED_IMAGE_DIR s3://$AWS_BUCKET_NAME/$UPLOADED_IMAGE_S3_DESTPATH
